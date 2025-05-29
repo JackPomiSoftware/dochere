a0390f5b411de22a71c00eb43a287dc6c55b6f17
MordXD <kotkinegor78@gmail.com>
2025-05-29 17:45:57 +0300

    изменил страницу result

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -3,62 +3,67 @@
     <div class="hero">
       <div class="heroimg">
         <table id="forms" cellpadding="1px">
+          <div class="search-inputs-background">
+            <tr>
+              <td colspan="2">
+                <input 
+                  type="text" 
+                  v-model="searchForm.symptome" 
+                  placeholder="Услуга, врач, клиника, симптом"
+                  id="symptome"
+                >
+              </td>
+              <td colspan="2">
+                <input 
+                  type="text" 
+                  v-model="searchForm.district" 
+                  placeholder="Метро, район, округ, город"
+                  id="district"
+                >
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input 
+                  type="text" 
+                  v-model="searchForm.priceFrom" 
+                  placeholder="Цена от, ₽"
+                  id="pricefrom"
+                >
+              </td>
+              <td>
+                <input 
+                  type="text" 
+                  v-model="searchForm.priceTo" 
+                  placeholder="до"
+                  id="priceto"
+                >
+              </td>
+              <td>
+                <input 
+                  :type="dateFieldType"
+                  v-model="searchForm.date" 
+                  placeholder="Когда"
+                  @focus="handleDateFocus"
+                  @blur="handleDateBlur"
+                  min="2025-06-01"
+                  id="date"
+                />
+              </td>
+              <td>
+                <button @click="search" id="find">Найти</button>
+              </td>
+            </tr>
+          </div>
           <tr>
-            <td colspan="4" style="height: 30px;"></td>
-            <td rowspan="4">
-              <img class="hero-image" src="/img/lizard.png">
-            </td>
-          </tr>
-          <tr>
-            <td colspan="2">
-              <input 
-                type="text" 
-                v-model="searchForm.symptome" 
-                placeholder="Услуга, врач, клиника, симптом"
-              >
-            </td>
-            <td colspan="2">
-              <input 
-                type="text" 
-                v-model="searchForm.district" 
-                placeholder="Метро, район, округ, город"
-              >
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <input 
-                type="text" 
-                v-model="searchForm.priceFrom" 
-                placeholder="Цена от, ₽"
-              >
-            </td>
-            <td>
-              <input 
-                type="text" 
-                v-model="searchForm.priceTo" 
-                placeholder="до"
-              >
-            </td>
-            <td>
-              <input 
-                type="date" 
-                v-model="searchForm.date" 
-                min="2025-06-01"
-              />
-            </td>
-            <td>
-              <button @click="search">Найти</button>
-            </td>
-          </tr>
-          <tr>
-            <td colspan="2" id="chx">
+            <td colspan="4" id="chx">
               <label for="children">
                 <input 
                   type="checkbox" 
                   v-model="searchForm.children" 
                   id="children"
                 >
+                <span class="checkmark"></span>
                 Детский врач
               </label>
               <label for="dms">
@@ -67,11 +72,13 @@
                   v-model="searchForm.dms" 
                   id="dms"
                 >
+                <span class="checkmark"></span>
                 По полису ДМС
               </label>
             </td>
           </tr>
         </table>
+        <img class="hero-image" src="/img/lizard.png">
       </div>
     </div>
     
@@ -116,12 +123,13 @@ export default {
   name: 'Results',
   data() {
     return {
+      dateFieldType: 'text', // Initial type for placeholder
       searchForm: {
         symptome: '',
         district: '',
         priceFrom: '',
         priceTo: '',
-        date: '',
+        date: '', // Initially empty for placeholder
         children: false,
         dms: false
       },
@@ -185,8 +193,21 @@ export default {
   },
   methods: {
     search() {
-      // Обновляем результаты поиска
-      console.log('Поиск с параметрами:', this.searchForm)
+      // Переход на страницу результатов с параметрами поиска
+      this.$router.push({
+        path: '/results',
+        query: this.searchForm
+      })
+    },
+    handleDateFocus() {
+      this.dateFieldType = 'date';
+    },
+    handleDateBlur() {
+      if (!this.searchForm.date) {
+        this.dateFieldType = 'text';
+      } else {
+        this.dateFieldType = 'date';
+      }
     },
     selectTime(doctor, time) {
       console.log(`Выбрано время ${time} у врача ${doctor.name}`)
